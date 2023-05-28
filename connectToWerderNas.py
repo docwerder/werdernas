bb27db796196f880c0a70394448ae751bbb055da
docwerder <wingbermuehle@icloud.com>
2023-05-28 14:38:36 +0200

    update connectToWerderNas WerderInsignia

--- connectToWerderNas.py
+++ connectToWerderNas.py
@@ -4,7 +4,7 @@ import signal
 from PySide2.QtGui import QPixmap
 from PySide2 import QtCore
 from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QApplication, QGridLayout, QMainWindow, QWidget, QPushButton, QLabel, QMessageBox, QFrame, QHBoxLayout
+from PySide2.QtWidgets import QApplication, QGridLayout, QMainWindow, QWidget, QPushButton, QLabel, QMessageBox, QFrame, QHBoxLayout, QVBoxLayout
 
 # Important: the next two lines have to be imported after the Pyside2 imports.
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas  # noqa: F401
@@ -46,7 +46,7 @@ def show_dialog_werderNASX(self):
 def show_dialog_werderNAS2X(self):
         msgBox = QMessageBox()
         msgBox.setIcon(QMessageBox.Information)
-        msgBox.setText("Connection to WERDERNASX successfull!")
+        msgBox.setText("Connection to WERDERNAS2X successfull!")
         msgBox.setWindowTitle("Information")
         msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
         returnValue = msgBox.exec()
@@ -92,6 +92,7 @@ def show_window_unmount_werderNAS2X(self):
         returnValue = msgBox.exec()
         if returnValue == QMessageBox.Ok:
             print('OK clicked')
+
 
 extra = {
     # Button colors
@@ -164,116 +165,158 @@ class QHLine(QFrame):
         self.setFrameShadow(QFrame.Sunken)
 
 
-class Main_WERDERNAS(QMainWindow):
-    def __init__(self, x_pos_main_gui, y_pos_main_gui, width_main_gui, height_main_gui, parent=None):
-        super(Main_WERDERNAS, self).__init__(parent)
+class Main_WERDERNAS(QWidget):
+    # def __init__(self, x_pos_main_gui, y_pos_main_gui, width_main_gui, height_main_gui):
+    def __init__(self):
+        super().__init__()
+        # super(Main_WERDERNAS, self).__init__(parent)
 
-        self.setWindowTitle('WERDERNAS GUI V0.5')
-        self.x_pos_main_gui = x_pos_main_gui
-        self.y_pos_main_gui = y_pos_main_gui
-        self.width_main_gui = width_main_gui
-        self.height_main_gui = height_main_gui
+        self.setWindowTitle('WERDERNAS GUI V0.7')
+        # self.x_pos_main_gui = x_pos_main_gui
+        # self.y_pos_main_gui = y_pos_main_gui
+        # self.width_main_gui = width_main_gui
+        # self.height_main_gui = height_main_gui
 
+        self.init_ui()
+    
+    def init_ui(self) -> None:
+              
         # For control
         # print('x_pos_main_pdw_gui_init: ', self.x_pos_main_pdw_gui)
         # print('y_pos_main_pdw_gui_init: ', self.y_pos_main_pdw_gui)
 
-        self.move(self.x_pos_main_gui, self.y_pos_main_gui)
+        # self.move(self.x_pos_main_gui, self.y_pos_main_gui)
         # print('width: ', self.width_main_gui)
         # print('height: ', self.height_main_gui)
+        
+        #% Create the complete layout:
+        grid_layout = QGridLayout()  
+        self.setLayout(grid_layout)
+        # self.complete_layout = QGridLayout()
 
-        # Define the main layout with buttons
-        # Define 'headline' consisting of header and logo ...
-        lbl_werdernas = QLabel('WERDERNAS GUI 0.6 !')
-        #lbl_werdernas.setProperty('class', '#ffd22b')
+        #% Define the main layout with buttons
+        #% Define 'headline' consisting of header and logo ...
+
+        lbl_werdernas = QLabel('WERDERNAS GUI 0.7 !')
         lbl_werdernas.setStyleSheet("font-size: 16px;" "color: #ffd22b;")
-        self.lbl_werder_logo = QLabel("Werder_logo")
-        self.werder_pixmap = QPixmap("werder_logo.png")
-        self.scaled_werder_logo = self.werder_pixmap.scaled(self.lbl_werder_logo.size() / 10, QtCore.Qt.KeepAspectRatio)
-        self.lbl_werder_logo.setPixmap(self.scaled_werder_logo)
-        self.lbl_werder_logo.setScaledContents(False)
+        lbl_werder_logo = QLabel("Werder_logo")
+        werder_pixmap = QPixmap("/Users/joerg/repos/werdernas/werder_logo.png")
+        scaled_werder_logo = werder_pixmap.scaled(lbl_werder_logo.size() / 10, QtCore.Qt.KeepAspectRatio)
+        lbl_werder_logo.setPixmap(scaled_werder_logo)
+        lbl_werder_logo.setScaledContents(False)
         
-        # Define Grid of pushbuttons... and their actions...
-        self.btn_connect_to_WerderNas = QPushButton('Mount To WERDERNAS')
-        # self.btn_connect_to_WerderNas.setProperty('class', '#ffd22b')
-        self.btn_connect_to_WerderNas.clicked.connect(mountToWerderNas.execute_applescript_werdernas)
-        mytext = "Connected to WERDERNAS successfull"
-        print(mytext)
-        self.btn_connect_to_WerderNas.clicked.connect(show_dialog_werderNAS)
+
+        #% Define Grid of pushbuttons... and their actions...
+
+        #% Mount WerderNAS
+        btn_connect_to_WerderNas = QPushButton('Mount To WERDERNAS')
+        btn_connect_to_WerderNas.clicked.connect(mountToWerderNas.execute_applescript_werdernas)
+        btn_connect_to_WerderNas.clicked.connect(show_dialog_werderNAS)
         
+        #% Unmount WerderNAS
+        btn_unmount_WerderNas = QPushButton('Unmount WERDERNAS')
+        btn_unmount_WerderNas.clicked.connect(unMountWerderNas.execute_applescript_unmount_werdernas)
+        btn_unmount_WerderNas.clicked.connect(show_window_unmount_werderNAS)
+
+        #% Mount WerderNAS2
+        btn_connect_to_WerderNas2 = QPushButton('Mount To WERDERNAS2')
+        btn_connect_to_WerderNas2.clicked.connect(mountToWerderNas2.execute_applescript_werdernas2)
+        btn_connect_to_WerderNas2.clicked.connect(show_dialog_werderNAS2)
+
+        #% Unmount WerderNAS2
+        btn_unmount_WerderNas2 = QPushButton('Unmount WERDERNAS2')
+        btn_unmount_WerderNas2.clicked.connect(unMountWerderNas2.execute_applescript_unmount_werdernas2)
+        btn_unmount_WerderNas2.clicked.connect(show_window_unmount_werderNAS2)
+
+        #% Mount WerderNASX
+        btn_connect_to_WerderNasx = QPushButton('Mount To WERDERNASX')
+        btn_connect_to_WerderNasx.clicked.connect(mountToWerderNasx.execute_applescript_werdernasx)
+        btn_connect_to_WerderNasx.clicked.connect(show_dialog_werderNASX)   
+
+        #% Unmount WerderNASX     
+        btn_unmount_WerderNasx = QPushButton('Unmount WERDERNASX')
+        btn_unmount_WerderNasx.clicked.connect(unMountWerderNasx.execute_applescript_unmount_werdernasx)
+        btn_unmount_WerderNasx.clicked.connect(show_window_unmount_werderNASX)
+
+        #% Mount WerderNAS2X        
+        btn_connect_to_WerderNas2x = QPushButton('Mount To WERDERNAS2X')       
+        btn_connect_to_WerderNas2x.clicked.connect(mountToWerderNas2x.execute_applescript_werdernas2x)
+        btn_connect_to_WerderNas2x.clicked.connect(show_dialog_werderNAS2X)     
+
+        #% Unmount WerderNAS2X
+        btn_unmount_WerderNas2x = QPushButton('Unmount WERDERNAS2X')
+        btn_unmount_WerderNas2x.clicked.connect(unMountWerderNas2x.execute_applescript_unmount_werdernas2x)
+        btn_unmount_WerderNas2x.clicked.connect(show_window_unmount_werderNAS2X)
+
         
-        self.btn_unmount_WerderNas = QPushButton('Unmount WERDERNAS')
-        self.btn_unmount_WerderNas.clicked.connect(unMountWerderNas.execute_applescript_unmount_werdernas)
-        self.btn_unmount_WerderNas.clicked.connect(show_window_unmount_werderNAS)
+        #% Define functions for mounting All
+        btn_mount_all = QPushButton('Mount All')
+        btn_mount_all.clicked.connect(mountToWerderNas.execute_applescript_werdernas)
+        btn_mount_all.clicked.connect(mountToWerderNas2.execute_applescript_werdernas2)
+        btn_mount_all.clicked.connect(mountToWerderNasx.execute_applescript_werdernasx)
+        btn_mount_all.clicked.connect(mountToWerderNas2x.execute_applescript_werdernas2x)
+        btn_mount_all.clicked.connect(show_dialog_werderNAS)
+        btn_mount_all.clicked.connect(show_dialog_werderNAS2)
+        btn_mount_all.clicked.connect(show_dialog_werderNASX)
+        btn_mount_all.clicked.connect(show_dialog_werderNAS2X)
 
-        # self.btn_connect_to_werdernas.clicked.connect(mountToWerderNas2)
-        # self.btn_connect_to_werdernas.clicked.connect(mountToWerderNas2x.execute_applescript_werdernas2x())
-        #self.btn_connect_to_werdernas.clicked.connect(self.okButtonClicked)
-        self.btn_connect_to_WerderNas2 = QPushButton('Mount To WERDERNAS2')
-        self.btn_connect_to_WerderNas2.clicked.connect(mountToWerderNas2.execute_applescript_werdernas2)
-        self.btn_connect_to_WerderNas2.clicked.connect(show_dialog_werderNAS2)
-        self.btn_unmount_WerderNas2 = QPushButton('Unmount WERDERNAS2')
-        self.btn_unmount_WerderNas2.clicked.connect(unMountWerderNas2.execute_applescript_unmount_werdernas2)
-        self.btn_unmount_WerderNas2.clicked.connect(show_window_unmount_werderNAS2)
+        #% Define functions for unmounting All
+        btn_unmount_all = QPushButton("Unmount All")
+        btn_unmount_all.clicked.connect(unMountWerderNas.execute_applescript_unmount_werdernas)
+        btn_unmount_all.clicked.connect(show_window_unmount_werderNAS)
+        btn_unmount_all.clicked.connect(unMountWerderNas2.execute_applescript_unmount_werdernas2)
+        btn_unmount_all.clicked.connect(show_window_unmount_werderNAS2)
+        btn_unmount_all.clicked.connect(unMountWerderNasx.execute_applescript_unmount_werdernasx)
+        btn_unmount_all.clicked.connect(show_window_unmount_werderNASX)
+        btn_unmount_all.clicked.connect(unMountWerderNas2x.execute_applescript_unmount_werdernas2x)
+        btn_unmount_all.clicked.connect(show_window_unmount_werderNAS2X)
 
-        # self.window_feature_overview = None
-        # self.btn_connect_to_werdernas2.clicked.connect(mountToWerderNas2)
+        btn_close = QPushButton("Close APP")
+        btn_close.clicked.connect(self.close)
 
-        self.btn_connect_to_WerderNasx = QPushButton('Mount To WERDERNASX')
-        self.btn_connect_to_WerderNasx.clicked.connect(mountToWerderNasx.
-        execute_applescript_werdernasx)
-        self.btn_connect_to_WerderNasx.clicked.connect(show_dialog_werderNASX)        
-        self.btn_unmount_WerderNasx = QPushButton('Unmount WERDERNASX')
-        self.btn_unmount_WerderNasx.clicked.connect(unMountWerderNasx.execute_applescript_unmount_werdernasx)
-        self.btn_unmount_WerderNasx.clicked.connect(show_window_unmount_werderNASX)
-
-        # self.window_feature_upload = None
-        # self.btn_connect_to_werdernasx.clicked.connect(mountToWerderNasx)
+        # self.window_show_feature_signals = None
+        #% Now: Define the Alignment of the buttons and labels via qgridlayout.
+        #% Define layout for horizontal line
+        # self.hor_line_1_layout = QHBoxLayout()
         
-        self.btn_connect_to_WerderNas2x = QPushButton('Mount To WERDERNAS2X')       
-        self.btn_connect_to_WerderNas2x.clicked.connect(mountToWerderNas2x.execute_applescript_werdernas2x)
-        self.btn_connect_to_WerderNas2x.clicked.connect(show_dialog_werderNAS2X)     
-        self.btn_unmount_WerderNas2x = QPushButton('Unmount WERDERNAS2X')
-        self.btn_unmount_WerderNas2x.clicked.connect(unMountWerderNas2x.execute_applescript_unmount_werdernas2x)
-        self.btn_unmount_WerderNas2x.clicked.connect(show_window_unmount_werderNAS2X)
-
-        self.window_show_feature_signals = None
-        # Now: Define the Alignment of the buttons and labels via qgridlayout.
-        # Define the layout-form, in which the widgets should be aligned...
-        # and add the widgets
-        # Define layout for horizontal line
-        self.hor_line_1_layout = QHBoxLayout()
-        # self.hor_line_1_layout.addWidget(QHLine())
-
-        layout = QGridLayout()
-        self.frame = QFrame()
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        # self.frame.setFrameShape(QFrame.VLine)
-        self.frame.setFrameShadow(QFrame.Sunken)
-        layout.addWidget(lbl_werdernas, 0, 0)
-        layout.addWidget(self.lbl_werder_logo, 0, 1, alignment=Qt.AlignRight)
-
-        layout.addWidget(self.btn_connect_to_WerderNas, 1, 0)
-        layout.addWidget(self.btn_unmount_WerderNas, 2, 0)
-
-        layout.addWidget(self.btn_connect_to_WerderNas2, 1, 1)
-        layout.addWidget(self.btn_unmount_WerderNas2, 2, 1)
+        # self.grid_layout = QGridLayout()
+        # self.frame = QFrame()
+        # self.frame.setFrameShape(QFrame.StyledPanel)
+        # # self.frame.setFrameShape(QFrame.VLine)
+        # self.frame.setFrameShadow(QFrame.Sunken)
         
-        layout.addWidget(QHLine(), 3, 0, 1, 2)
-        
-        layout.addWidget(self.btn_connect_to_WerderNasx, 4, 0)
-        layout.addWidget(self.btn_unmount_WerderNasx, 5, 0)
+        grid_layout.addWidget(lbl_werdernas, 0, 0)
+        grid_layout.addWidget(lbl_werder_logo, 0, 1, alignment=Qt.AlignRight)
 
-        layout.addWidget(self.btn_connect_to_WerderNas2x, 4, 1)
-        layout.addWidget(self.btn_unmount_WerderNas2x, 5, 1)
+        grid_layout.addWidget(btn_connect_to_WerderNas, 1, 0)
+        grid_layout.addWidget(btn_unmount_WerderNas, 2, 0)
+
+        grid_layout.addWidget(btn_connect_to_WerderNas2, 1, 1)
+        grid_layout.addWidget(btn_unmount_WerderNas2, 2, 1)
         
-        layout.addWidget(QHLine(), 3, 0, 1, 2)
+        grid_layout.addWidget(QHLine(), 3, 0, 1, 2)
         
+        grid_layout.addWidget(btn_connect_to_WerderNasx, 4, 0)
+        grid_layout.addWidget(btn_unmount_WerderNasx, 5, 0)
+
+        grid_layout.addWidget(btn_connect_to_WerderNas2x, 4, 1)
+        grid_layout.addWidget(btn_unmount_WerderNas2x, 5, 1)
+        
+        grid_layout.addWidget(QHLine(), 6, 0, 1, 2)
+        
+        grid_layout.addWidget(btn_mount_all, 7, 0)
+        grid_layout.addWidget(btn_unmount_all, 7, 1)
+
+        grid_layout.addWidget(btn_close, 8, 0, 1, 2)
+        # grid_layout.addWidget(btn_unmount_WerderNasx, 8, 0)
+
+        # grid_layout.addWidget(btn_connect_to_WerderNasx, 7, 1)
+        # grid_layout.addWidget(btn_unmount_WerderNasx, 8, 1)
         # layout.addWidget(self.btn_unmount_WerderNas2x, 4, 0)
         # layout.setRowStretch(1, 2)
         
         # layout.addWidget(self.frame, 2, 0)
-
+        # self.btn_connect_to_WerderNas.setProperty('class', '#ffd22b')
         # Define the scretching of the rows...
         # Dont how exactly how it works...
         # layout.setRowStretch(0, 10)
@@ -281,9 +324,11 @@ class Main_WERDERNAS(QMainWindow):
         # layout.setRowStretch(2, 6)
 
         # Showing all the widgets via the "logic" of the qwidget
-        dummy_widget = QWidget()
-        dummy_widget.setLayout(layout)
-        self.setCentralWidget(dummy_widget)
+        # self.complete_layout.addLayout(self.grid_layout)
+        # self.setLayout(self.complete_layout)
+        # dummy_widget = QWidget()
+        # dummy_widget.setLayout(layout)
+        # self.setCentralWidget(dummy_widget)
 
         # Dont know, how this value is about 639?
         # width_dw = dummy_widget.frameGeometry().width()
@@ -369,8 +414,8 @@ if __name__ == '__main__':
     
     app = QApplication(sys.argv)
     apply_stylesheet(app, theme='dark_amber.xml', extra=extra)
-    window = Main_WERDERNAS(200, 150, 250, 150)
-
+    # window = Main_WERDERNAS(200, 150, 150, 150)
+    window = Main_WERDERNAS()
     # For example: Get the width of the window, BUT its problematic...
     # width_main_window = window.width()
     # print('width_mw: ', width_main_window)
